7e6fade268250b74495a1acee9b59d1b2aedeb9b
velklusia <116886960+velklusia@users.noreply.github.com>
2024-09-04 14:54:12 +0200

    It worked indeed 🐳🐬

--- BE/calculator.py
+++ BE/calculator.py
@@ -45,15 +45,15 @@ if not args.rest and len(sys.argv) != 4:
 
 if (args.add):
     result = CalculatorHelper().add(args.add[0], args.add[1])
-    print(f'I INDEED WORKED 🦇{args.add[0]}+{args.add[1]}={result}')
+    print(f'I INDEED WORKED 🦇 {args.add[0]}+{args.add[1]}={result}')
 elif (args.subtract):
     result = CalculatorHelper().subtract(args.subtract[0], args.subtract[1])
-    print(f'I INDEED WORKED 🦝{args.subtract[0]}-{args.subtract[1]}={result}')
+    print(f'I INDEED WORKED 🦝 {args.subtract[0]}-{args.subtract[1]}={result}')
 elif (args.multiply):
     result = CalculatorHelper().multiply(args.multiply[0], args.multiply[1])
-    print(f'I INDEED WORKED 🐜{args.multiply[0]}*{args.multiply[1]}={result}')
+    print(f'I INDEED WORKED 🐜 {args.multiply[0]}*{args.multiply[1]}={result}')
 elif (args.divide):
     result = CalculatorHelper().divide(args.divide[0], args.divide[1])
-    print(f'I INDEED WORKED 🐺{args.divide[0]}/{args.divide[1]}={result}')
+    print(f'I INDEED WORKED 🐺 {args.divide[0]}/{args.divide[1]}={result}')
 elif (args.rest):
     calculator_rest_service.main(args)
