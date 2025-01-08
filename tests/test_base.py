6dd22a9f7867680f7737079e6469775ff6afc909
velklusia <116886960+velklusia@users.noreply.github.com>
2025-01-08 04:37:48 +0100

    :)

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,4 @@
 import pytest
-
 from BE.calculator_helper import CalculatorHelper
 from calculator_client.client import Client
 
@@ -7,8 +6,11 @@ BASE_URL = "http://localhost:5000"
 
 class BaseTestCalculator:
     def setup_method(self, method):
+        """Setup method to run before every test."""
         self.calculator = CalculatorHelper()
         self.client = Client(base_url=BASE_URL)
 
     def teardown_method(self, method):
+        """Teardown method to run after every test."""
         del self.calculator
+        del self.client
